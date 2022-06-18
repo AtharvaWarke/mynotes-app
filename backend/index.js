6b013de184080a4a8d45e76f51f58a0900296dd2
AtharvaWarke <avwarke@asu.edu>
2022-06-18 23:35:39 +0530

    Routes commit 1

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,11 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-	res.send("Hello Worldw!");
-});
+app.use(express.json())
+
+//Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
