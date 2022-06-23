aefd116553ebedce170085ad2aeabbd2eb724f50
AtharvaWarke <avwarke@asu.edu>
2022-06-23 20:13:24 +0530

    Navbar-1-react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<>
+			<Navbar />
+		</>
+	);
 }
 
 export default App;
