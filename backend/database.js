2239a5c3f3ce27d8b0f0ffe1f07b82c15e03a953
AtharvaWarke <avwarke@asu.edu>
2022-07-18 14:13:09 +0530

    mongoAtlas-connect

--- backend/database.js
+++ backend/database.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/mynotes-app";
+const mongoURI =
+	"mongodb+srv://admin:u9U4HVpmHZSecKHt@cluster0.wkizw0g.mongodb.net/mynotes-app?retryWrites=true&w=majority";
 
 const connectToMongo = () => {
-	mongoose.connect(mongoURI, () => {
-		console.log("Connected");
-	});
+	mongoose.connect(mongoURI);
 };
 
 module.exports = connectToMongo;
